c2569afeb6d6939b28877183352976b160ef4dae
Joao1397 <joseadasilvar13@gmail.com>
2025-03-20 22:49:36 -0400

    Second Commit

--- C1/run_mesh.sh
+++ C1/run_mesh.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-
 foamCleanTutorials
 rm -r 0 > /dev/null 2>&1
 rm *.obj > /dev/null 2>&1
-blockMesh 					| tee log.blockMesh
-surfaceFeatures 				| tee log.surfaceFeatures
-#snappyHexMesh -noFunctionObjects -overwrite     | tee log.snappyHexMesh
-snappyHexMesh -noFunctionObjects                | tee log.snappyHexMesh
-checkMesh -latestTime				| tee log.checkMesh	
+blockMesh | tee log.blockMesh
+surfaceFeatures | tee log.surfaceFeatures
+#snappyHexMesh -overwrite | tee log.snappyHexMesh
+snappyHexMesh | tee log.snappyHexMesh
+checkMesh -latestTime | tee log.checkMesh
+paraFoam -builtin 
